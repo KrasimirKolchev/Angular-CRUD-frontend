aee24beb06f2a4f878937f70d00ef88b6d57789e
KrasimirKolchev <krasimir.kolchev2087@gmail.com>
2021-12-03 15:38:34 +0200

    Revert "initial commit"
    This reverts commit 8fb431f9de2dab4ca80d97f0e56f8ec080dfa701.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from "./app-routing.module";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {OfferService} from "./services/offer-service";
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {OffersAllComponent} from './components/offers-all/offers-all.component';
-import {OfferAddComponent} from './components/offer-add/offer-add.component';
-import { OfferDetailsComponent } from './components/offer-details/offer-details.component';
-import { OfferUpdateComponent } from './components/offer-update/offer-update.component';
-import { BrandsAllComponent } from './components/brands-all/brands-all.component';
-import { BrandAddComponent } from './components/brand-add/brand-add.component';
-import { ModelsAllComponent } from './components/models-all/models-all.component';
-import { ModelAddComponent } from './components/model-add/model-add.component';
-import {BrandService} from "./services/brand-service";
-import {ModelService} from "./services/model-service";
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OffersAllComponent,
-    OfferAddComponent,
-    OfferDetailsComponent,
-    OfferUpdateComponent,
-    BrandsAllComponent,
-    BrandAddComponent,
-    ModelsAllComponent,
-    ModelAddComponent
+    AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatToolbarModule,
-        ReactiveFormsModule
-    ],
-  providers: [BrandService, ModelService, OfferService],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
